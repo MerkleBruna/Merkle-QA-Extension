0c5496f10ef979545d2c5f5aaecf21ed5eb74874
Bruna Barbosa De Souza <bruna.barbosadesouza@merkleinc.com.au>
2022-05-24 14:12:26 +1000

    Remove unused code

--- inject-script.js
+++ inject-script.js
@@ -1,35 +1,13 @@
-// var port = chrome.runtime.connect({
-//     extensionId: string,
-//     connectInfo: object
-// })
-
-// window.addEventListener("message", (event) => {
-//   // We only accept messages from ourselves
-//   if (event.source != window) {
-//     return;
-//   }
-
-//   if (event.data.type && (event.data.type == "FROM_PAGE")) {
-//     console.log("Content script received: " + event.data.text);
-//     port.postMessage(event.data.text);
-//   }
-// }, false);
-
-// document.getElementById("switch").addEventListener("click", () => {
-//     window.postMessage({ type: "FROM_PAGE", text: "Hello from the webpage!" }, "*");
-//   }, false);
-
-
 function checkWindowData() {
-    let main = {}
-    // main.performance = JSON.parse(JSON.stringify(window.performance)) || null;
-    main.adobeTarget = JSON.parse(JSON.stringify(adobe.target)) || null;
-    return main
+  let main = {}
+  // main.performance = JSON.parse(JSON.stringify(window.performance)) || null;
+  main.adobeTarget = JSON.parse(JSON.stringify(adobe.target)) || null
+  return main
 }
 
 setInterval(() => {
-    console.log("message interval running");
-    let target = checkWindowData();
-    console.log(target);
-    window.postMessage({ type: "FROM_PAGE", target }, "*")
-}, 5000);
+  console.log('message interval running')
+  let target = checkWindowData()
+  console.log(target)
+  window.postMessage({ type: 'FROM_PAGE', target }, '*')
+}, 5000)
